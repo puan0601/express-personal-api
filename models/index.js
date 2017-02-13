84c2815034980ff2e5fd9f78561e8dd61a1c5c3d
puan0601 <anton.pugach@gmail.com>
2017-02-13 04:13:12 -0800

    small test 1

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
 var mongoose = require("mongoose");
 mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/antons-personal-api");
 
-module.exports.Venture = require("./venture.js");
+module.exports.Venture = require('./venture.js');
