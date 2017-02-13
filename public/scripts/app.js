21cefb6a4e131db2e4a2271fb4ed99679e467696
puan0601 <anton.pugach@gmail.com>
2017-02-13 05:05:53 -0800

    hopefully this works

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,66 +1,4 @@
 console.log("Sanity Check: JS is working!");
-function getVentureHtml(venture) {
-  return `<hr>
-  <p>
-  <b>${venture.title}</b>
-  by ${venture.description}
-  <button type="button" name="button" class="deleteBtn btn btn-danger pull-right" data-id=${venture._id}>Delete</button>
-  </p>`;
-}
-
-function getAllVenturesHtml(ventures) {
-  return ventures.map(getVentureHtml).join("");
-}
-// helper function to render all posts to view
-// note: we empty and re-render the collection each time our post data changes
-function render () {
-  // empty existing posts from view
-  $venturesList.empty();
-
-  // pass `allVentures` into the template function
-  var venturesHtml = getAllVenturesHtml(allVentures);
-
-  // append html to the view
-  $venturesList.append(venturesHtml);
-}
-
-function handleSuccess(json) {
-  allVentures = json;
-  render();
-}
-
-function handleError(e) {
-  console.log('uh oh');
-  $('#ventureTarget').text('Failed to load ventures, is the server working?');
-}
-
-function newVentureSuccess(json) {
-  $('#newVentureForm input').val('');
-  allVentures.push(json);
-  render();
-}
-
-function newVentureError() {
-
-}
-
-function deleteVentureSuccess(json) {
-  var venture = json;
-  var ventureId = venture._id;
-
-  // find the venture with the correct ID and remove it from our allVentures array
-  for(var index = 0; index < allVentures.length; index++) {
-    if(allVentures[index]._id === ventureId) {
-      allVentures.splice(index, 1);
-      break;  // we found our venture - no reason to keep searching (this is why we didn't use forEach)
-    }
-  }
-  render();
-}
-
-function deleteVentureError() {
-
-}
 
 var $venturesList;
 var allVentures = [];
@@ -95,4 +33,67 @@ $(document).ready(function(){
       error: deleteVentureError
     });
   });
+
+  function getVentureHtml(venture) {
+    return `<hr>
+            <p>
+              <b>${venture.title}</b>
+              by ${venture.description}
+              <button type="button" name="button" class="deleteBtn btn btn-danger pull-right" data-id=${venture._id}>Delete</button>
+            </p>`;
+  }
+
+  function getAllVenturesHtml(ventures) {
+    return ventures.map(getVentureHtml).join("");
+  }
+  // helper function to render all posts to view
+  // note: we empty and re-render the collection each time our post data changes
+  function render () {
+    // empty existing posts from view
+    $venturesList.empty();
+
+    // pass `allVentures` into the template function
+    var venturesHtml = getAllVenturesHtml(allVentures);
+
+    // append html to the view
+    $venturesList.append(venturesHtml);
+  }
+
+  function handleSuccess(json) {
+    allVentures = json;
+    render();
+  }
+
+  function handleError(e) {
+    console.log('uh oh');
+    $('#ventureTarget').text('Failed to load ventures, is the server working?');
+  }
+
+  function newVentureSuccess(json) {
+    $('#newVentureForm input').val('');
+    allVentures.push(json);
+    render();
+  }
+
+  function newVentureError() {
+
+  }
+
+  function deleteVentureSuccess(json) {
+    var venture = json;
+    var ventureId = venture._id;
+
+    // find the venture with the correct ID and remove it from our allVentures array
+    for(var index = 0; index < allVentures.length; index++) {
+      if(allVentures[index]._id === ventureId) {
+        allVentures.splice(index, 1);
+        break;  // we found our venture - no reason to keep searching (this is why we didn't use forEach)
+      }
+    }
+    render();
+  }
+
+  function deleteVentureError() {
+
+  }
 });
