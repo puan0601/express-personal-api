df049968a3f7d5bd2298020c9e34fed4a9d5ef0f
puan0601 <anton.pugach@gmail.com>
2017-02-13 03:49:13 -0800

    trying to get heroku to work

--- models/index.js
+++ models/index.js
@@ -1,6 +1,4 @@
 var mongoose = require("mongoose");
 mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/antons-personal-api");
 
-var Venture = require('./venture');
-
 module.exports.Venture = require('./venture.js');
